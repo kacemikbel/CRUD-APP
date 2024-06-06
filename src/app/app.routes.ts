e670e1c8e9f7ee6b462848dff8e04b72e02aa7b2
Kacem ikbel <kassemikbel56@gmail.com>
2024-06-06 16:04:05 +0200

    .

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,11 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './page/login/login.component';
 import { RegisterComponent } from './page/register/register.component';
 
-
 export const routes: Routes = [ 
     {
         path: '', component:LayoutComponent,
         children: [
-            {path: '', redirectTo : "Home" , pathMatch :"full"},
+            {path: '', redirectTo : "home", pathMatch :"full"},
             {path: 'home', component : HomeComponent},
             {path: 'login', component : LoginComponent},
             {path: 'register', component : RegisterComponent}
